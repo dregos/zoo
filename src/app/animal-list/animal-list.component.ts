a1916ea7d77fc10a4a3908a84676857bd6b96409
Daniel <dregojevic@gmail.com>
2017-08-22 16:35:49 +0200

    remove button and unknown for empty birth date

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -23,14 +23,39 @@ export class AnimalListComponent {
         birthDate: '01.03.1989'
       },
       {
+        species: 'Penguin',
+        name: 'Pingu',
+        birthDate: '08.03.2006'
+      },
+      {
         species: 'Panda',
         name: 'Polly',
         birthDate: '08.03.2006'
       },
       {
+        species: 'Panda',
+        name: 'Blush',
+        birthDate: ''
+      },
+      {
         species: 'Zebra',
         name: 'Ziggy',
         birthDate: '22.04.2000'
+      },
+      {
+        species: 'Bear',
+        name: 'Smokey',
+        birthDate: '16.10.2010'
+      },
+      {
+        species: 'Monkey',
+        name: 'Molly',
+        birthDate: '14.11.2002'
+      },
+      {
+        species: 'Monkey',
+        name: 'Bush',
+        birthDate: ''
       }
     ]
   }
